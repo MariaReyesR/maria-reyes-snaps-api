f233fa63128c29a6d963aa3a5505c9fbf8611fe0
Maria Reyes <paolareyesrivero@gmail.com>
2025-03-02 18:35:43 -0500

    initial setup of routes/tags.js

---
+++ routes/tags.js
@@ -0,0 +1,14 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+const router = express.Router();
+const tagsPath = path.join(process.cwd(), "data/tags.json");
+
+const getTags = () => JSON.parse(fs.readFileSync(tagsPath));
+
+router.get("/", (req, res) => {
+  res.json(getTags());
+});
+
+export default router;

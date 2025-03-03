1a50ee71835583cbeb6c5c76499d2ad9e6d17e1e
Maria Reyes <paolareyesrivero@gmail.com>
2025-03-02 19:32:39 -0500

    add get comment to photo.js

--- routes/photos.js
+++ routes/photos.js
@@ -1,11 +1,19 @@
 import express from "express";
 import fs from "fs";
 import path from "path";
+import crypto from "crypto";
 
 const router = express.Router();
+router.use(express.json());
 const photosPath = path.join(process.cwd(), "data/photos.json");
 
-const getPhotos = () => JSON.parse(fs.readFileSync(photosPath));
+const getPhotos = () => {
+  const photos = JSON.parse(fs.readFileSync(photosPath));
+  return photos.map((photo) => ({
+    ...photo,
+    comments: photo.comments || [],
+  }));
+};
 
 router.get("/", (req, res) => {
   res.json(getPhotos());
@@ -20,6 +28,18 @@ router.get("/:id", (req, res) => {
     res.status(404).json({ message: "Photo not found" });
   } else {
     res.json(photo);
+  }
+});
+
+router.get("/:id/comments", (req, res) => {
+  const photos = getPhotos();
+  const photoId = req.params.id;
+  const photo = photos.find((p) => p.id === photoId);
+
+  if (!photo) {
+    res.status(404).json({ message: "Photo not found" });
+  } else {
+    res.json(photo.comments || []);
   }
 });
 

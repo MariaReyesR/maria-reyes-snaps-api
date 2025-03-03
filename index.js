6210522b018bb6510c0838f231afa63b65917478
Maria Reyes <paolareyesrivero@gmail.com>
2025-03-02 19:52:07 -0500

    add static image files

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
